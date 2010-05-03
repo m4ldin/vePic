de5010df8f241ca16dd4856838b66c1011fb9d0e
Erik Österlund <fisksvett@gmail.com>
2010-05-03 16:40:01 +0200

    Stupid dummy.

--- vePic/app/controllers/upload_controller.rb
+++ vePic/app/controllers/upload_controller.rb
@@ -3,7 +3,13 @@ class UploadController < ApplicationController
      render :file => 'app/views/upload/uploadfile.rhtml'
   end
   def uploadFile
-    post = DataFile.save(params[:upload])
+    path = DataFile.save(params[:upload])
+    image = Image.new
+    image.path = path
+    image.title = "Erik äger."
+    image.author = "Erik"
+    image.slide_id = 1
+    image.save
     render :text => "File has been uploaded successfully"
   end
 end
